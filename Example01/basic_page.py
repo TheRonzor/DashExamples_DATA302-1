45449c1f54de7833b48a1fed8ca93cafccc3c7cf
Ron <ronniecougar@gmail.com>
2024-04-17 13:16:02 -0400

    done with example 1

--- Example01/basic_page.py
+++ Example01/basic_page.py
@@ -11,20 +11,11 @@ from datetime import datetime as dt
 import numpy as np
 
 TITLE = 'Hello World!'
+FIG_ID = 'my-first-figure'
 
+TAU = 2*np.pi
 
-def apply_main_layout(app: Dash) -> None:
-    layout = html.Div(id='main-div',
-                      children = [
-                          html.H1('Welcome to my website!'),
-                          html.Hr()
-                          ]
-    )
-    app.layout = layout
-    return
-
-
-if __name__ == '__main__':
+def run_app() -> None:
     my_app = Dash(__name__)
     print(f'Started running at {str(dt.now())}')
     
@@ -37,3 +28,31 @@ if __name__ == '__main__':
     # Run the app in debug mode
     # Don't have debug mode turned on when you send someone (i.e. Ron) your code
     my_app.run(debug=True)
+
+    return
+
+def make_figure() -> go.Figure:
+    x = np.linspace(0, 10, 500)
+    y = np.sin(TAU*x)
+
+    fig = px.scatter(None,x,y)
+
+    return fig
+
+def apply_main_layout(app: Dash) -> None:
+    layout = html.Div(id='main-div',
+                      children = [
+                          html.H1('Welcome to my website!'),
+                          html.Hr(),
+                          html.H2('Here is a figure!'),
+                          dcc.Graph(id=FIG_ID,
+                                    figure=make_figure()
+                                    )
+                          ]
+    )
+    app.layout = layout
+    return
+
+
+if __name__ == '__main__':
+    run_app()
